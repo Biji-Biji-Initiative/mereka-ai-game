ad877b507015401d51f87fcb325576409de72c72
aslyheer <0iamhira@gmail.com>
2025-04-22 21:20:55 +0545

    feat: firebase is added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://api.yourserver.com/api'
+  production: true,
+  apiUrl: 'http://localhost:3000/api',
+  firebase: {
+    apiKey: "AIzaSyD7esOuznHY_x0r1XsalWSZBoOJTspuABA",
+    authDomain: "mereka-ai-game.firebaseapp.com",
+    projectId: "mereka-ai-game",
+    storageBucket: "mereka-ai-game.firebasestorage.app",
+    messagingSenderId: "939508804715",
+    appId: "1:939508804715:web:71ccc7faab3879aff5552e",
+    measurementId: "G-NJWRDG4PWP"
+  }
 };
